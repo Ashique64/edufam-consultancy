ad2e329f10c278d3f7edbcb02e9ae0e6eab83e8c
Ashique <ashique9496@gmail.com>
2025-12-09 11:35:01 +0530

    Lenis implemented

--- frontend/src/components/SmoothScroll/SmoothScroll.js
+++ frontend/src/components/SmoothScroll/SmoothScroll.js
@@ -3,7 +3,7 @@ import { ReactLenis } from "lenis/react";
 
 function SmoothScroll({ children }) {
     return (
-        <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+        <ReactLenis root options={{ lerp: 0.05, duration: 2, smoothWheel: true }}>
             {children}
         </ReactLenis>
     );
