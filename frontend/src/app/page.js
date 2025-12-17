db04aa4707de52b1b8edfaeba27c468da1586b86
Ashique <ashique9496@gmail.com>
2025-12-17 14:35:41 +0530

    add visa in the why choose section

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,16 +3,12 @@
 import { useState, useEffect } from "react";
 import Preloader from "@/components/Preloader/Preloader";
 import Navbar from "@/components/Navbar/Navbar";
-import Hero2 from "@/components/Hero2/Hero2";
-import Course2 from "@/components/Course2/Course2";
 import Courses from "@/components/Courses/Courses";
 import SuccessStories from "@/components/SuccessStories/SuccessStories";
 import HowItWork from "@/components/HowItWork/HowItWork";
 import WhyChoose from "@/components/WhyChoose/WhyChoose";
-import WhyChoose2 from "@/components/WhyChoose2/WhyChoose2";
 import Contact from "@/components/Contact/Contact";
 import Footer from "@/components/Footer/Footer";
-import SuccessStories2 from "@/components/SuccessStories2/SuccessStories2";
 import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
@@ -39,14 +35,10 @@ export default function Home() {
             <div style={{ opacity: loading ? 0 : 1, transition: "opacity 0.5s ease-in-out" }}>
                 <Navbar />
                 <Hero />
-                {/* <Hero2 /> */}
                 <Courses />
-                {/* <Course2 /> */}
                 <SuccessStories />
-                {/* <SuccessStories2 /> */}
                 <HowItWork />
                 <WhyChoose />
-                {/* <WhyChoose2 /> */}
                 <Contact />
                 <Footer />
             </div>
